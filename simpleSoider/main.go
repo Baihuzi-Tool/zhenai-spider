3cfb211be18de409f14105bdeb8bb05ffe90fd02
baihuzi <baihuzi8@gmail.com>
2019-08-16 15:18:59 +0800

    add

--- simpleSoider/main.go
+++ simpleSoider/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"zhenaiSpider/simpleSoider/engine"
+	"zhenaiSpider/simpleSoider/parser/zhenai"
+)
+
 func main() {
-	$END$
+	testRequest := engine.Request{
+		Url:        "http://www.zhenai.com/zhenghun/",
+		ParserFunc: zhenai.ParseCityList,
+	}
+
+	engine.Run(testRequest)
 }
--- simpleSoider/parser/zhenai/citylist.go
+++ simpleSoider/parser/zhenai/citylist.go
@@ -1,1 +1,24 @@
 package zhenai
+
+import (
+	"regexp"
+	"zhenaiSpider/simpleSoider/engine"
+)
+
+const cityListRe = `<a [^href]*href="(http://www.zhenai.com/zhenghun/[a-z0-9]+)"[^>]*>([^<]*)</a>`
+
+func ParseCityList(content []byte) engine.ParserResult {
+	re := regexp.MustCompile(cityListRe)
+	submatch := re.FindAllSubmatch(content, -1)
+
+	result := engine.ParserResult{}
+	for _, m := range submatch {
+		result.Items = append(result.Items, string(m[2]))
+		result.Requests = append(result.Requests, engine.Request{
+			Url:        string(m[1]),
+			ParserFunc: engine.NilParser,
+		})
+	}
+
+	return result
+}
